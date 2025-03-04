4fb49a83d1249766286cc711f091cd1181156d7c
Amindu Sathsara <dapsathsara@std.appsc.sab.ac.lk>
2025-03-05 00:41:11 +0530

    add get all and get userby emailID and  _id

--- src/app.js
+++ src/app.js
@@ -32,6 +32,55 @@ app.post("/createUser", async (req, res)=> {
     });
 
 
+    //End points for getting all the users 
+    app.get('/getUsers',async (req, res) => {
+        try{
+          const users=await User.find({});
+          res.send(users);
+        }catch(err){
+          res.status(500).send("Can't find your friends here , Mah boy");
+        }
+      })
+
+    //End point for getting a single user by emailID
+    app.get('/getUser',async (req, res) => {
+        const userEmail=req.body.emailID;
+      
+        try{
+          const user=await User.findOne({emailID:userEmail});
+          /*if(user.lenght===0){
+            return res.status(404).send("User not found");  //this two approaches are same I just add for now only
+          }*/
+          if(!user){
+            return res.status(404).send("User not found",);
+          }
+      
+          res.send(user);
+        }catch(err){
+          res.status(404).send("User not found");
+        }
+      })
+
+
+      //Once the authentication add to this it is very usefull to have get user by _id ,
+      app.get('/getUserById/:id',async (req, res) => {
+        const userId=req.body._id;
+      
+        try{
+          const user=await User.findById({_id:userId});
+          if(!user){
+            return res.status(404).send("User not found");
+          }
+      
+          res.send(user);
+        }catch(err){
+          res.status(500).send("Can't find your friend here, Mah boy");
+        }
+      })
+      
+
+
+
     connectToDB().then(() => {
         console.log("Database connected");
         app.listen(5555, () => {
